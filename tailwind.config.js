1ee3e7c0b01786d1a7470ccacdc073f8855ce98e
ouis-nsli <ouis.suaeid.nasli@gmail.com>
2024-12-03 18:43:47 +0400

    Add tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3556AB",
+          dark: "#071D55",
+          darker: "#0D2972",
+        },
+        secondary: {
+          DEFAULT: "#CDE53D",
+          dark: "#9EB031",
+        },
+        neutral: {
+          light: "#E7E7E7",
+          DEFAULT: "#CBCBCB",
+          dark: "#8D8D8D",
+          white: "#FDFDFD",
+        },
+        success: {
+          DEFAULT: "#53DA69",
+          dark: "#49C25D",
+          darker: "#399649",
+        },
+        danger: {
+          DEFAULT: "#AB3535",
+          dark: "#720D0D",
+        },
+      },
+      boxShadow: {
+        "inset-soft": "0px 3px 1px 0px #A8B5DE80 inset",
+        "soft-black": "0px 4px 4px 0px #00000040",
+        "solid-black": "0px 2px 0px 0px #000000",
+        "subtle-black": "0px 4px 4px 0px #0000001A",
+      },
+    },
   },
   plugins: [],
 };
